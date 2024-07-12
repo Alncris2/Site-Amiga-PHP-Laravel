4271c5b259287a0bf78438567569614c53be0345
alncris2 <alan@nsolucoes.digital>
2024-07-11 22:20:23 -0300

    bugfix: arrumando projeto

--- config/database.php
+++ config/database.php
@@ -45,22 +45,24 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
+            'url' => env('DB_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'amiga'),
-            'username' => env('DB_USERNAME', 'amiga'),
-            'password' => env('DB_PASSWORD', '@miga565'),
-            'unix_socket' => env('DB_SOCKET', '/Applications/MAMP/tmp/mysql/mysql.sock'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
+            'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
             'prefix_indexes' => true,
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
+            'set_timezone' => 'America/Sao_Paulo',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))",
+            ]) : [PDO::MYSQL_ATTR_INIT_COMMAND => "SET sql_mode=(SELECT REPLACE(@@sql_mode,'ONLY_FULL_GROUP_BY',''))"],
         ],
 
         'pgsql' => [
--- config/seo.php
+++ config/seo.php
@@ -1,10 +1,8 @@
 <?php
 
 return [
-
     'titulo'=>'Amiga',
     'descricao'=>'ASSOCIAÇÃO DE CRIANÇAS E ADOLESCENTES DE IGARAPAVA',
     'imagem'=>'public\lib\img\padrãopng.png',
-    'url'=>'http://amigaigarapava.com.br'
-
+    'url'=>'https://amigaigarapava.com.br'
 ];
